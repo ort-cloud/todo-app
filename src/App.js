061dbabd51cc5d79de1bcbb3d8d73baec006c75c
Carl Ortutay <carlorty@gmail.com>
2020-08-27 16:09:23 -0400

    list items dynamic using dummy data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
 import TodoItem from "./TodoItem";
+import todosData from "./todosData";
 
 function App() {
+  const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
+
   return (
     <div className='todo-list'>
       <h1>Todo</h1>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      {todoItems}
     </div>
   );
 }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import './App.css';
+import React from "react";
+import "./App.css";
+import todosData from "./todosData";
 
-function TodoItem () {
+function TodoItem(props) {
   return (
     <div className="todo-item">
-      <input type="checkbox"/>
-      <p>Placeholder text</p>
+      <input type="checkbox" />
+      <p>{props.item.text}</p>
     </div>
   );
 }
 
-export default TodoItem
+export default TodoItem;
